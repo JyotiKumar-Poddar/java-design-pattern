a4dcbd5b1ea557fa9df91160cc379018008918a2
jyotipoddar <jyoti.johney@gmail.com>
2018-01-16 19:15:29 +0200

    removed final variable

--- src/main/java/com/java/designpattern/decorator/Beverage.java
+++ src/main/java/com/java/designpattern/decorator/Beverage.java
@@ -2,7 +2,7 @@ package com.java.designpattern.decorator;
 
 public abstract class Beverage {
 
-    final  String description = "no description found";
+    String description = "no description found";
 
     public String getDescription() {
         return description;
